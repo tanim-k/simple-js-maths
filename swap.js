88a458f1563381fa74c5d665a28c5dec817a7011
tanim-k <tanimkhan877@gmail.com>
2022-02-01 11:37:53 +0600

    swapping swapping

--- swap.js
+++ swap.js
@@ -1,8 +1,11 @@
+// stearted learning swapping using a temporary variable:::::
 let first = 9;
 let second = 19;
 console.log(first, second);
+// time to swap::::🙌
 let temp = first;
 first = second;
 second = temp;
 console.log(first, second);
+// just enterchanging the position of first and second:🌹😁 it'll do the same as awap xD:: ✔✔✔
 console.log(second, first);
