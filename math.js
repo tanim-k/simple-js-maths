ee3e5e9bb99751cee4eb2599c6ee490bbe5fc54d
tanim-k <tanimkhan877@gmail.com>
2022-02-01 10:22:40 +0600

    maths practicet

--- math.js
+++ math.js
@@ -9,7 +9,17 @@
 // const output = Math.ceil(myNumberAny);
 // console.log(output);
 
-const myNumber = 99.98765434;
-const output = Math.ceil(myNumber);
-console.log(output);
+// const myNumber = 99.98765434;
+// const output = Math.floor(myNumber);
+// console.log(output);
 
+// const myNumber = 93.98765434;
+// const output = Math.round(myNumber);
+// console.log(output);
+
+// const myNumber = 67.165434;
+// const output = Math.round(myNumber);
+// console.log(output);
+
+const output = Math.random();
+console.log(output)
