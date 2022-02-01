fea2e5b153554e1c491262eb48b657d4b21ed123
tanim-k <tanimkhan877@gmail.com>
2022-02-01 12:30:47 +0600

    finding max value

---
+++ max.js
@@ -0,0 +1,15 @@
+const bussiness = 500;
+const minister = 1000;
+if(bussiness > minister) {
+    console.log('businessman is bigger, though his amount is less than the minister person');
+}
+else{
+    console.log('ministerperson er amount beshi, halay beshi powerful just because of if conditional is not true')
+}
+
+if(minister > bussiness) {
+    console.log('businessman is bigger, though his amount is less than the minister person. its because of if conditioonal is true');
+}
+else{
+    console.log('ministerperson er amount beshi tarporo halay kom powerful just because of if conditionals')
+}
