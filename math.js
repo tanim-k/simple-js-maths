c30ae7bb222b065783b354e3365d2f61ace1c0e0
tanim-k <tanimkhan877@gmail.com>
2022-02-01 09:41:24 +0600

    maths practice in javascript

---
+++ math.js
@@ -0,0 +1,15 @@
+// .abs = absolute 
+// .ceil = ceilling a number
+
+// const myNumber = -10;
+// const output = Math.abs(myNumber);
+// console.log(output);
+
+// const myNumberAny = 67.8954;
+// const output = Math.ceil(myNumberAny);
+// console.log(output);
+
+const myNumber = 99.98765434;
+const output = Math.ceil(myNumber);
+console.log(output);
+
