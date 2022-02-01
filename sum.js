cac08e2fbc0a5143a4388a8c5ba8f8f7817c3e0d
tanim-k <tanimkhan877@gmail.com>
2022-02-01 17:07:15 +0600

    adding array elements by a function

--- sum.js
+++ sum.js
@@ -7,3 +7,15 @@ for(let i=0; i<numbers.length; i++) {
     console.log(element);
     console.log('the addition of sum and next element of the array:', sum); 
 }
+// a function for adding array elements //love
+function arrayTotal(numbers){
+    let sum = 0;
+    for(i = 0; i<numbers.length; i++) {
+        const element = numbers[i];
+        sum = sum + element;
+    }
+    return sum ;
+}
+// the arrayTotal function calling//
+const total = arrayTotal(numbers);
+console.log('array total:', total);
