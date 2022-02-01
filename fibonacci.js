9f8270f96a4386cefb9dd478ab6521d848d273b5
tanim-k <tanimkhan877@gmail.com>
2022-02-01 20:14:06 +0600

    fibonacci seris meking  by a function

---
+++ fibonacci.js
@@ -0,0 +1,26 @@
+// fibonacci seris //
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 //
+// any varse of fibonacci//
+// 3nd=2nd+1st
+// 4nd=3rd+2nd
+// 6th=5th+4th
+// 12th=10th+11th
+// nth=(n-1)th + (n-2)th
+// ith = (i-1)th + (i-2)th
+
+const fibo = [0, 1];
+for(let i = 2; i<= 10; i++) {
+    fibo[i] = fibo[i-1] + fibo[i-2];
+}
+console.log('fibonacci seris:', fibo);
+
+// a function for fibonacci seris //
+function fibonacciSeris(num) {
+    const fibo = [0, 1];   
+    for(i = 2; i <= num; i++){
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo;
+}
+const fiboSeris = fibonacciSeris(15);
+console.log('fibonacci seris array:', fiboSeris);
