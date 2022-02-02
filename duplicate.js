bb1c6ecbcd73712386c1376978c7261b44206b36
tanim-k <tanimkhan877@gmail.com>
2022-02-02 09:16:43 +0600

    no duplicate

---
+++ duplicate.js
@@ -0,0 +1,18 @@
+const names = ['habul', 'kabul', 'babul', 'fabul', 'jabul', 'habul', 'abul', 'kabul', 'fabul', 'gabul', 'rabul', 'ibul', 'ebul', 'qubul', 'tabul', 'ebul']
+function removeDuplicate(names) {
+    const unique = [];
+    // for (i = 0; i < names.length; i++) {
+    //     const element = names[i];
+    //     console.log(element);
+    // }
+    for (const element of names) {
+        console.log(element);
+        if ( unique.indexOf(element) == -1) {
+            unique.push(element);
+        }
+    }
+    return unique;
+}
+// no removeDuplicate;icates in the new array /./.
+const uniqueNames = removeDuplicate(names);
+console.log(uniqueNames)
