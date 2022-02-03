5d600482d3e4ccdd4d1df719ce48e817886d7990
tanim-k <tanimkhan877@gmail.com>
2022-02-03 09:40:34 +0600

    lal hoiya gese ek samanno debug korte

---
+++ reverse-string.js
@@ -0,0 +1,13 @@
+const greetings = 'hello, how are you?';  
+
+function reverseString(text) {
+    let reverse = '';  
+    for (const letter of text) {
+        console.log(letter)
+        reverse = letter + reverse;
+    }
+    return reverse;
+}
+const reversed = reverseString(greetings);
+console.log(reversed);
+ 
