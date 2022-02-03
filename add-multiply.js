e3e404a304b805f3dc132580a148e044676d756d
tanim-k <tanimkhan877@gmail.com>
2022-02-03 10:23:51 +0600

    adding

---
+++ add-multiply.js
@@ -0,0 +1,11 @@
+// add two numbers by a function //
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+const firstTotaal = add(55, 78);
+console.log(firstTotaal);
+const secondTotaal = add(554, 784);
+console.log(secondTotaal);
+const thirdTotaal = add(5555, 7668);
+console.log(thirdTotaal);
